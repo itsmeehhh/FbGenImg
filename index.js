edcc458ff297c81e447d6f08125c2ec55e347436
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-05-19 20:23:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,6 +81,7 @@ const attachment = message.message.attachments[0]
             });
         userStatus[senderId] = false;
       } catch (c2) {
+          try {
         userStatus[senderId] = true;
     fetch(url).then(res => res.buffer()).then(buffer => {
         jadianime(buffer.toString('base64')).then(tuanime => {
@@ -96,7 +97,7 @@ const attachment = message.message.attachments[0]
       userStatus[senderId] = false;
     } catch (e) {
 botly.sendText({id: senderId, text: "البوت تحت الصيانة الان 🚨❤️"});
-        }}
+        }}}
     } else if (message.message.attachments[0].type == "audio") {
       botly.sendText({id: senderId, text: "يرجى ارسال الصور فقط ❤️"});
         } else if (message.message.attachments[0].type == "video") {
